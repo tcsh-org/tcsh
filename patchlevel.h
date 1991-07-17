ea42893446440aeb47882d855f6057771c61d418
Christos Zoulas <christos@zoulas.com>
1991-07-17 21:54:59 +0000

    6.00.02

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.00/RCS/patchlevel.h,v 3.0 1991/07/04 21:54:11 christos Exp $ */
+/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.00/RCS/patchlevel.h,v 3.1 1991/07/15 19:37:24 christos Exp christos $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -8,7 +8,7 @@
 #define ORIGIN "Cornell"
 #define REV 6
 #define VERS 00
-#define PATCHLEVEL 01
+#define PATCHLEVEL 02
 #define DATE "07/15/91"
 
 #endif /* _h_patchlevel */
