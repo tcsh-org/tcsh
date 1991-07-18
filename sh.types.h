67cd52e7b211948a676b3bd819722a11c4bb38d6
Christos Zoulas <christos@zoulas.com>
1991-07-18 02:57:19 +0000

    Added pyramid

--- sh.types.h
+++ sh.types.h
@@ -1,4 +1,4 @@
-/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.00/RCS/sh.types.h,v 3.2 1991/07/08 05:22:44 christos Exp $ */
+/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.00/RCS/sh.types.h,v 3.3 1991/07/15 19:37:24 christos Exp $ */
 /* sh.types.h: Do the necessary typedefs for each system.
  *             Up till now I avoided making this into a separate file
  *	       But I just wanted to eliminate the whole mess from sh.h
@@ -207,6 +207,17 @@ extern char *sbrk();
 /*   typedef unsigned int speed_t; */
 # endif /* _SPEED_T */
 #endif /* RENO */
+
+
+/***
+ *** Pyramid, BSD universe
+ *** In addition to the size_t
+ ***/
+#ifdef pyr
+# ifndef _PID_T
+#  define _PID_T
+# endif /* _PID_T */
+#endif /* pyr */
 
 
 /***
