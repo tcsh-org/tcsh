64c819745bd9c4f1eebe17824a1da5046c3f7b3d
Christos Zoulas <christos@zoulas.com>
1991-08-05 23:12:02 +0000

    Another 6.00.02

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.00/RCS/patchlevel.h,v 3.2 1991/07/17 21:54:59 christos Exp christos $ */
+/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.00/RCS/patchlevel.h,v 3.3 1991/07/28 17:48:10 christos Exp christos $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -9,6 +9,6 @@
 #define REV 6
 #define VERS 00
 #define PATCHLEVEL 02
-#define DATE "07/24/91"
+#define DATE "08/05/91"
 
 #endif /* _h_patchlevel */
