540ea844a78e018471575cb2e798f5fe0cd2420b
Christos Zoulas <christos@zoulas.com>
1991-11-26 04:36:30 +0000

    patch 5

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.00/RCS/patchlevel.h,v 3.17 1991/11/17 05:39:06 christos Exp $ */
+/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.00/RCS/patchlevel.h,v 3.18 1991/11/22 02:28:12 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -9,7 +9,7 @@
 #define REV 6
 #define VERS 00
 #define PATCHLEVEL 04
-#define DATE "11/21/91"
-#define LOCALSTR " (beta 4)"
+#define DATE "11/25/91"
+#define LOCALSTR " (beta 5)"
 
 #endif /* _h_patchlevel */
