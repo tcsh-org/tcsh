a89c6306ea1fbc106b45947525ea321a3c084899
Christos Zoulas <christos@zoulas.com>
1992-03-08 02:19:22 +0000

    *** empty log message ***

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.01/RCS/patchlevel.h,v 3.27 1992/02/13 05:28:51 christos Exp $ */
+/* $Header: /home/hyperion/mu/christos/src/sys/tcsh-6.01/RCS/patchlevel.h,v 3.28 1992/02/21 23:16:20 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -8,7 +8,7 @@
 #define ORIGIN "Cornell"
 #define REV 6
 #define VERS 1
-#define PATCHLEVEL 5
-#define DATE "02/21/92"
+#define PATCHLEVEL 6
+#define DATE "03/07/92"
 
 #endif /* _h_patchlevel */
