71c95f11b933e1d274d7d28f3973a926bc257bd4
Christos Zoulas <christos@zoulas.com>
1993-06-25 21:22:35 +0000

    *** empty log message ***

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /u/christos/src/tcsh-6.03/RCS/patchlevel.h,v 3.61 1993/06/07 14:29:35 christos Exp $ */
+/* $Header: /u/christos/src/tcsh-6.03/RCS/patchlevel.h,v 3.62 1993/06/11 20:53:15 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -8,7 +8,7 @@
 #define ORIGIN "Cornell"
 #define REV 6
 #define VERS 3
-#define PATCHLEVEL 9
+#define PATCHLEVEL 10
 #define DATE "93/06/11"
 
 #endif /* _h_patchlevel */
