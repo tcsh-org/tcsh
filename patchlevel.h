24782cb10b0e1cb4d707d4cfde4351007ca97d8d
Christos Zoulas <christos@zoulas.com>
1994-02-04 15:14:01 +0000

    *** empty log message ***

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /u/christos/src/tcsh-6.04/RCS/patchlevel.h,v 3.71 1993/12/19 21:38:00 christos Exp $ */
+/* $Header: /u/christos/src/tcsh-6.04/RCS/patchlevel.h,v 3.72 1994/01/31 16:04:49 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -9,6 +9,6 @@
 #define REV 6
 #define VERS 4
 #define PATCHLEVEL 7
-#define DATE "94/01/31"
+#define DATE "94/02/04"
 
 #endif /* _h_patchlevel */
