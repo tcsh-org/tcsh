e2c98ef652c85c93a864e8145faec822123dd8b1
Christos Zoulas <christos@zoulas.com>
2000-11-12 01:36:16 +0000

    getaddrinfo is not enough to make sure that we have ipv6; we need sockaddr_storage too. RH6 has the first but not the second.

--- sh.h
+++ sh.h
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/sh.h,v 3.90 2000/07/04 19:41:50 christos Exp $ */
+/* $Header: /src/pub/tcsh/sh.h,v 3.91 2000/11/11 23:03:37 christos Exp $ */
 /*
  * sh.h: Catch it all globals and includes file!
  */
@@ -393,6 +393,9 @@ typedef int sigret_t;
 # include <netinet/in.h>
 # include <arpa/inet.h>
 # include <sys/socket.h>
+# if defined(_SS_SIZE) || defined(_SS_MAXSIZE)
+#  define INET6
+# endif
 # include <sys/uio.h>	/* For struct iovec */
 #endif /* REMOTEHOST */
 
