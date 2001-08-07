5ecf8a4eb9d8e6547961020e496481403974c679
Christos Zoulas <christos@zoulas.com>
2001-08-07 00:29:56 +0000

    prepare for 6.10.02

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/patchlevel.h,v 3.126 2000/11/19 20:50:43 christos Exp $ */
+/* $Header: /src/pub/tcsh/patchlevel.h,v 3.127 2001/04/26 20:22:36 kim Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 10
-#define PATCHLEVEL 1
-#define DATE "2001-04-26"
+#define PATCHLEVEL 2
+#define DATE "2001-08-06"
 
 #endif /* _h_patchlevel */
