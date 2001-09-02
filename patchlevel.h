e2cae3e26024dda26c256723676e9e2217dbf412
Christos Zoulas <christos@zoulas.com>
2001-09-02 21:16:25 +0000

    Final version changes and Announcement message.

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/patchlevel.h,v 3.127 2001/04/26 20:22:36 kim Exp $ */
+/* $Header: /src/pub/tcsh/patchlevel.h,v 3.128 2001/08/07 00:29:56 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -7,8 +7,8 @@
 
 #define ORIGIN "Astron"
 #define REV 6
-#define VERS 10
-#define PATCHLEVEL 2
-#define DATE "2001-08-06"
+#define VERS 11
+#define PATCHLEVEL 0
+#define DATE "2001-09-02"
 
 #endif /* _h_patchlevel */
