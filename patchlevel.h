7c0a31b3b29b747ce0158023e4f02176b11c3b55
Christos Zoulas <christos@zoulas.com>
2002-07-01 21:07:44 +0000

    goto 11.03.

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/patchlevel.h,v 3.130 2002/03/08 18:49:55 christos Exp $ */
+/* $Header: /src/pub/tcsh/patchlevel.h,v 3.131 2002/05/30 15:22:09 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 11
-#define PATCHLEVEL 02
-#define DATE "2002-05-16"
+#define PATCHLEVEL 03
+#define DATE "2002-07-01"
 
 #endif /* _h_patchlevel */
