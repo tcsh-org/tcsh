f6317531062249e11ec8f5465765d74fe77430ca
Christos Zoulas <christos@zoulas.com>
2003-08-04 16:19:13 +0000

    put back ignoreeof the way it was historically and documented in the man page.

--- sh.set.c
+++ sh.set.c
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/sh.set.c,v 3.44 2002/07/01 20:50:22 christos Exp $ */
+/* $Header: /src/pub/tcsh/sh.set.c,v 3.45 2003/07/29 21:19:44 christos Exp $ */
 /*
  * sh.set.c: Setting and Clearing of variables
  */
@@ -32,13 +32,14 @@
  */
 #include "sh.h"
 
-RCSID("$Id: sh.set.c,v 3.44 2002/07/01 20:50:22 christos Exp $")
+RCSID("$Id: sh.set.c,v 3.45 2003/07/29 21:19:44 christos Exp $")
 
 #include "ed.h"
 #include "tw.h"
 
 extern Char HistLit;
 extern bool GotTermCaps;
+int numeof = 0;
 
 static	void		 update_vars	__P((Char *));
 static	Char		*getinx		__P((Char *, int *));
@@ -122,7 +123,7 @@ update_vars(vp)
 	ed_Init();		/* reset the editor */
     }
     else if (eq(vp, STRhome)) {
-	register Char *cp;
+	Char *cp;
 
 	cp = Strsave(varval(vp));	/* get the old value back */
 
@@ -147,6 +148,18 @@ update_vars(vp)
     else if (eq(vp, STRshlvl)) {
 	tsetenv(STRKSHLVL, varval(vp));
     }
+    else if (eq(vp, STRignoreeof)) {
+	Char *cp;
+	numeof = 0;
+    	for ((cp = varval(STRignoreeof)); cp && *cp; cp++) {
+	    if (!Isdigit(*cp)) {
+		numeof = 0;
+		break;
+	    }
+	    numeof = numeof * 10 + *cp - '0';
+	}
+	if (numeof <= 0) numeof = 26;	/* Sanity check */
+    } 
     else if (eq(vp, STRbackslash_quote)) {
 	bslash_quote = 1;
     }
@@ -732,6 +745,8 @@ unset(v, c)
 	HIST = '!';
 	HISTSUB = '^';
     }
+    if (adrof(STRignoreeof) == 0)
+	numeof = 0;
     if (adrof(STRpromptchars) == 0) {
 	PRCH = '>';
 	PRCHROOT = '#';
