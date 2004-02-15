3dc81e2400ca0dcb4dc61f01ff30cfc904a1b6c1
Amol Deshpande <amol.vinayak.deshpande@gmail.com>
2004-02-15 00:43:17 +0000

    update version to current

--- win32/version.h
+++ win32/version.h
@@ -1,13 +1,13 @@
-/*$Header: /src/pub/tcsh/win32/version.h,v 1.7 2003/11/04 01:37:44 amold Exp $*/
+/*$Header: /src/pub/tcsh/win32/version.h,v 1.8 2004/02/13 22:27:13 christos Exp $*/
 #ifndef VERSION_H
 #define VERSION_H
 
 /* remember to change both instance of the version -amol */
 
 #ifdef NTDBG
-#define LOCALSTR ",nt-rev-7.07-debug"
+#define LOCALSTR ",nt-rev-7.09-debug"
 #else
-#define LOCALSTR ",nt-rev-7.07" //changed ordonly test in nt_open
+#define LOCALSTR ",nt-rev-7.09" //changed ordonly test in nt_open
 								//patches
 #endif NTDBG
 
