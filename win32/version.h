094f26c2b959848aaa9368a93cef4f9660590bd4
Amol Deshpande <amol.vinayak.deshpande@gmail.com>
2004-07-25 14:36:27 +0000

    Fixed crash in native code for `foo`, where foo was not found.

--- win32/version.h
+++ win32/version.h
@@ -1,13 +1,13 @@
-/*$Header: /src/pub/tcsh/win32/version.h,v 1.9 2004/02/15 00:43:17 amold Exp $*/
+/*$Header: /src/pub/tcsh/win32/version.h,v 1.10 2004/05/19 18:22:28 christos Exp $*/
 #ifndef VERSION_H
 #define VERSION_H
 
 /* remember to change both instance of the version -amol */
 
 #ifdef NTDBG
-#define LOCALSTR ",nt-rev-7.09-debug"
+#define LOCALSTR ",nt-rev-7.10-debug"
 #else
-#define LOCALSTR ",nt-rev-7.09" //changed ordonly test in nt_open
+#define LOCALSTR ",nt-rev-7.10" 
 								//patches
 #endif NTDBG
 
