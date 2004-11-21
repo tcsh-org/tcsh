a6d0e927e60c41d70e600a5b65740a01eb912828
Christos Zoulas <christos@zoulas.com>
2004-11-21 04:48:24 +0000

    ready fo 6.13.04

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/patchlevel.h,v 3.141 2004/08/04 17:14:16 christos Exp $ */
+/* $Header: /src/pub/tcsh/patchlevel.h,v 3.142 2004/11/20 20:30:47 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 13
-#define PATCHLEVEL 03
+#define PATCHLEVEL 04
 #define DATE "2004-11-20"
 
 #endif /* _h_patchlevel */
