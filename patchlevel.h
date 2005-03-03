529f361350b0c95b0425032649850877999fbc2b
Kimmo Suominen <kimmo@suominen.com>
2005-03-03 00:43:58 +0000

    Prepare for 6.13.08 and 6.14.00

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/patchlevel.h,v 3.145 2005/01/05 16:20:28 christos Exp $ */
+/* $Header: /src/pub/tcsh/patchlevel.h,v 3.146 2005/01/18 20:46:10 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 13
-#define PATCHLEVEL 07
-#define DATE "2005-01-18"
+#define PATCHLEVEL 08
+#define DATE "2005-03-03"
 
 #endif /* _h_patchlevel */
