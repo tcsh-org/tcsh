43ebc9c0ab9d37f3e1b2d3bf7a3ed013ebe2afc8
Kimmo Suominen <kimmo@suominen.com>
2005-04-11 22:53:18 +0000

    Release 6.14.01

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/patchlevel.h,v 3.150 2005/03/21 21:50:55 kim Exp $ */
+/* $Header: /src/pub/tcsh/patchlevel.h,v 3.151 2005/03/25 17:36:08 kim Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 14
-#define PATCHLEVEL 0
-#define DATE "2005-03-25"
+#define PATCHLEVEL 1
+#define DATE "2005-04-11"
 
 #endif /* _h_patchlevel */
