d7b3e8174c0b7c1b7cae224d551bb9bb3afd08b4
Christos Zoulas <christos@zoulas.com>
2005-04-14 14:01:23 +0000

    PR/12: gumbygo: Symlink expansion fails. Disable strict testing introduced in 6.10.00. Explanation in-line.

--- sh.dir.c
+++ sh.dir.c
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/sh.dir.c,v 3.66 2005/03/03 16:40:53 kim Exp $ */
+/* $Header: /src/pub/tcsh/sh.dir.c,v 3.67 2005/04/11 22:10:56 kim Exp $ */
 /*
  * sh.dir.c: Directory manipulation functions
  */
@@ -33,7 +33,7 @@
 #include "sh.h"
 #include "ed.h"
 
-RCSID("$Id: sh.dir.c,v 3.66 2005/03/03 16:40:53 kim Exp $")
+RCSID("$Id: sh.dir.c,v 3.67 2005/04/11 22:10:56 kim Exp $")
 
 /*
  * C Shell - directory management
@@ -316,21 +316,30 @@ dnormalize(Char *cp, int expnd)
 	/*
 	 * count the number of "../xxx" or "xxx/../xxx" in the path
 	 */
-	for (dp=start; *dp && *(dp+1); dp++)
+	for (dp = start; *dp && *(dp + 1); dp++)
 	    if (IS_DOTDOT(start, dp))
 	        dotdot++;
+
 	/*
 	 * if none, we are done.
 	 */
         if (dotdot == 0)
-	    return (Strsave(cp));
-
+	    return (Strsave(start));
+	
+# ifdef notdef
 	/*
+	 * We disable this test because:
+	 * cd /tmp; mkdir dir1 dir2; cd dir2; ln -s /tmp/dir1; cd dir1;
+	 * echo ../../dir1 does not expand. We had enabled this before
+	 * because it was bothering people with expansions in compilation
+	 * lines like -I../../foo. Maybe we need some kind of finer grain
+	 * control?
+	 *
 	 * If the path doesn't exist, we are done too.
 	 */
-	if (lstat(short2str(cp), &sb) != 0 && errno == ENOENT)
-	    return (Strsave(cp));
-	
+	if (lstat(short2str(start), &sb) != 0 && errno == ENOENT)
+	    return (Strsave(start));
+# endif
 
 	cwd = (Char *) xmalloc((size_t) (((int) Strlen(dcwd->di_name) + 3) *
 					   sizeof(Char)));
@@ -349,7 +358,7 @@ dnormalize(Char *cp, int expnd)
 	/*
 	 * Ignore . and count ..'s
 	 */
-	for (;;) {
+	for (cp = start;;) {
 	    dotdot = 0;
 	    buf[0] = '\0';
 	    dp = buf; 
