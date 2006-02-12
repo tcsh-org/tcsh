8c6416fde7a0d2bb74c97d1c114971a2cf260101
Christos Zoulas <christos@zoulas.com>
2006-02-12 17:26:41 +0000

    update the date.

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/patchlevel.h,v 3.153 2005/04/12 23:58:47 kim Exp $ */
+/* $Header: /src/pub/tcsh/patchlevel.h,v 3.154 2006/01/13 16:20:38 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -9,6 +9,6 @@
 #define REV 6
 #define VERS 14
 #define PATCHLEVEL 3
-#define DATE "2006-01-13"
+#define DATE "2006-02-12"
 
 #endif /* _h_patchlevel */
