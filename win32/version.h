82284b676922802db618155663be376a108b00d8
Amol Deshpande <amol.vinayak.deshpande@gmail.com>
2006-06-26 04:41:48 +0000

    Cleanup win32 dependency rules in makefile. increment nt version.

--- win32/version.h
+++ win32/version.h
@@ -1,13 +1,13 @@
-/*$Header: /p/tcsh/cvsroot/tcsh/win32/version.h,v 1.18 2006/04/13 00:59:03 amold Exp $*/
+/*$Header: /p/tcsh/cvsroot/tcsh/win32/version.h,v 1.19 2006/04/29 07:19:09 amold Exp $*/
 #ifndef VERSION_H
 #define VERSION_H
 
 /* remember to change both instance of the version -amol */
 
 #ifdef NTDBG
-#define LOCALSTR ",nt-rev-8.04-debug"
+#define LOCALSTR ",nt-rev-8.05-debug"
 #else
-#define LOCALSTR ",nt-rev-8.04" 
+#define LOCALSTR ",nt-rev-8.05" 
 								//patches
 #endif NTDBG
 
