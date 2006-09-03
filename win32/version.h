cbcf7052946efc3f5f4eb5db85b2d426c72c1d77
Amol Deshpande <amol.vinayak.deshpande@gmail.com>
2006-09-03 16:11:46 +0000

    update nt version to reflect fix for script execution.

--- win32/version.h
+++ win32/version.h
@@ -1,13 +1,13 @@
-/*$Header: /p/tcsh/cvsroot/tcsh/win32/version.h,v 1.19 2006/04/29 07:19:09 amold Exp $*/
+/*$Header: /p/tcsh/cvsroot/tcsh/win32/version.h,v 1.20 2006/06/26 04:41:48 amold Exp $*/
 #ifndef VERSION_H
 #define VERSION_H
 
 /* remember to change both instance of the version -amol */
 
 #ifdef NTDBG
-#define LOCALSTR ",nt-rev-8.05-debug"
+#define LOCALSTR ",nt-rev-8.06-debug"
 #else
-#define LOCALSTR ",nt-rev-8.05" 
+#define LOCALSTR ",nt-rev-8.06" 
 								//patches
 #endif NTDBG
 
