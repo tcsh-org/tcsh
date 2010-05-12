f6bd1ca73328a78932bd0a5e9df79b4eb590f4c1
Christos Zoulas <christos@zoulas.com>
2010-05-12 15:01:52 +0000

    Add histlen parameter to enthist(). Constify setNS() parameter. Add displayHistStats() (Ted Anderson)

--- sh.decls.h
+++ sh.decls.h
@@ -1,4 +1,4 @@
-/* $Header: /p/tcsh/cvsroot/tcsh/sh.decls.h,v 3.57 2010/01/26 20:03:17 christos Exp $ */
+/* $Header: /p/tcsh/cvsroot/tcsh/sh.decls.h,v 3.58 2010/05/08 00:41:58 christos Exp $ */
 /*
  * sh.decls.h	 External declarations from sh*.c
  */
@@ -212,11 +212,12 @@ extern  int	  	  t_pmatch	(const Char *, const Char *,
  * sh.hist.c
  */
 extern	void	 	  dohist	(Char **, struct command *);
-extern  struct Hist 	 *enthist	(int, struct wordent *, int, int);
+extern  struct Hist 	 *enthist	(int, struct wordent *, int, int, int);
 extern	void	 	  savehist	(struct wordent *, int);
 extern	char		 *fmthist	(int, ptr_t);
 extern	void		  rechist	(Char *, int);
 extern	void		  loadhist	(Char *, int);
+extern	void		  displayHistStats(const char *);
 
 /*
  * sh.init.c
@@ -382,7 +383,7 @@ extern	void		  setq		(const Char *, Char **,
 extern	void		  unset		(Char **, struct command *);
 extern	void		  unset1	(Char *[], struct varent *);
 extern	void		  unsetv	(Char *);
-extern	void		  setNS		(Char *);
+extern	void		  setNS		(const Char *);
 extern	void		  shift		(Char **, struct command *);
 extern	void		  plist		(struct varent *, int);
 extern	Char		 *unparse	(struct command *);
