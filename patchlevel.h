593a688fe6a5c256ee2c41d1d47df4ecc13011fa
Christos Zoulas <christos@zoulas.com>
2012-02-13 16:33:50 +0000

    welcome to 6.18.01

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /p/tcsh/cvsroot/tcsh/patchlevel.h,v 3.178 2012/01/05 16:59:19 christos Exp $ */
+/* $Header: /p/tcsh/cvsroot/tcsh/patchlevel.h,v 3.179 2012/01/14 16:25:09 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 18
-#define PATCHLEVEL 0
-#define DATE "2012-01-14"
+#define PATCHLEVEL 1
+#define DATE "2012-02-14"
 
 #endif /* _h_patchlevel */
