0af7225cfcb8f5fb53df670c7487d970456296f9
Christos Zoulas <christos@zoulas.com>
2012-12-15 23:20:25 +0000

    Revert 3.79; breaks: cat << _EOF > huh #!/bin/sh echo "[$@]" echo "I am running" echo "I am running ($$)" >> huh.out _EOF cat << _EOF > huh.tcsh #!/bin/tcsh -f
    ./huh \
        `#comment blah blah blah` \
        parameter a \
        `#comment blah blah blah` \
        parameter b \
        `#comment blah blah blah` \
        parameter c
    echo ok
    _EOF

--- sh.glob.c
+++ sh.glob.c
@@ -1,4 +1,4 @@
-/* $Header: /p/tcsh/cvsroot/tcsh/sh.glob.c,v 3.81 2011/02/25 23:19:11 christos Exp $ */
+/* $Header: /p/tcsh/cvsroot/tcsh/sh.glob.c,v 3.82 2011/02/27 00:15:17 christos Exp $ */
 /*
  * sh.glob.c: Regular expression expansion
  */
@@ -32,7 +32,7 @@
  */
 #include "sh.h"
 
-RCSID("$tcsh: sh.glob.c,v 3.81 2011/02/25 23:19:11 christos Exp $")
+RCSID("$tcsh: sh.glob.c,v 3.82 2011/02/27 00:15:17 christos Exp $")
 
 #include "tc.h"
 #include "tw.h"
@@ -781,8 +781,6 @@ backeval(struct blk_buf *bb, struct Strbuf *word, Char *cp, int literal)
 		stderror(ERR_OLD);
 	    alias(&paraml);
 	    t = syntax(paraml.next, &paraml, 0);
-	    if (t == NULL)
-		return;
 	    cleanup_push(t, syntax_cleanup);
 	    /* The F_BACKQ flag must set so the job output is correct if
 	     * printexitvalue is set.  If it's not set, the job output
