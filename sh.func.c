4094558af300197e938a50b505714430477c7eff
Christos Zoulas <christos@zoulas.com>
2013-10-31 12:58:36 +0000

    support fore limits from various BSD's (Stefan Neudorf)

--- sh.func.c
+++ sh.func.c
@@ -1,4 +1,4 @@
-/* $Header: /p/tcsh/cvsroot/tcsh/sh.func.c,v 3.166 2013/01/24 13:32:09 christos Exp $ */
+/* $Header: /p/tcsh/cvsroot/tcsh/sh.func.c,v 3.167 2013/01/24 13:44:10 christos Exp $ */
 /*
  * sh.func.c: csh builtin functions
  */
@@ -32,7 +32,7 @@
  */
 #include "sh.h"
 
-RCSID("$tcsh: sh.func.c,v 3.166 2013/01/24 13:32:09 christos Exp $")
+RCSID("$tcsh: sh.func.c,v 3.167 2013/01/24 13:44:10 christos Exp $")
 
 #include "ed.h"
 #include "tw.h"
@@ -1945,6 +1945,14 @@ struct limits limits[] =
     { RLIMIT_NOFILE, 	"descriptors", 1,	""		},
 # endif /* RLIMIT_NOFILE */
 
+# ifdef RLIMIT_NPTS
+    { RLIMIT_NPTS,	"pseudoterminals", 1,	""		},
+# endif /* RLIMIT_NPTS */
+
+# ifdef RLIMIT_KQUEUES
+    { RLIMIT_KQUEUES,	"kqueues",	1,	""		},
+# endif /* RLIMIT_KQUEUES */
+
 # ifdef RLIMIT_CONCUR
     { RLIMIT_CONCUR, 	"concurrency", 1,	"thread(s)"	},
 # endif /* RLIMIT_CONCUR */
@@ -1956,6 +1964,10 @@ struct limits limits[] =
 # ifdef RLIMIT_NPROC
     { RLIMIT_NPROC,	"maxproc",	1,	""		},
 # endif /* RLIMIT_NPROC */
+
+# ifdef RLIMIT_NTHR
+    { RLIMIT_NTHR,	"threads",	1,	""		},
+# endif /* RLIMIT_NTHR */
 
 # if defined(RLIMIT_OFILE) && !defined(RLIMIT_NOFILE)
     { RLIMIT_OFILE,	"openfiles",	1,	""		},
@@ -1972,6 +1984,10 @@ struct limits limits[] =
 # ifdef RLIMIT_LOCKS 
     { RLIMIT_LOCKS,	"maxlocks",	1,	""		}, 
 # endif /* RLIMIT_LOCKS */ 
+
+# ifdef RLIMIT_POSIXLOCKS
+    { RLIMIT_POSIXLOCKS,"posixlocks",	1,	""		},
+# endif /* RLIMIT_POSIXLOCKS */
 
 # ifdef RLIMIT_SIGPENDING 
     { RLIMIT_SIGPENDING,"maxsignal",	1,	""		}, 
