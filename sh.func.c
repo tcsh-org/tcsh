934ab995d462824bdc68c028baf1c5591dd85d33
Christos Zoulas <christos@zoulas.com>
2015-05-04 17:10:45 +0000

    Put it back the way it was in tcsh-6.18.01

--- sh.func.c
+++ sh.func.c
@@ -1,4 +1,4 @@
-/* $Header: /p/tcsh/cvsroot/tcsh/sh.func.c,v 3.171 2014/07/22 21:53:00 christos Exp $ */
+/* $Header: /p/tcsh/cvsroot/tcsh/sh.func.c,v 3.172 2015/05/04 15:28:12 christos Exp $ */
 /*
  * sh.func.c: csh builtin functions
  */
@@ -32,7 +32,7 @@
  */
 #include "sh.h"
 
-RCSID("$tcsh: sh.func.c,v 3.171 2014/07/22 21:53:00 christos Exp $")
+RCSID("$tcsh: sh.func.c,v 3.172 2015/05/04 15:28:12 christos Exp $")
 
 #include "ed.h"
 #include "tw.h"
@@ -1274,9 +1274,6 @@ xecho(int sep, Char **v)
 		    c = '\\';
 		    break;
 		case '0':
-		case '1':
-		case '2':
-		case '3':
 		    c = 0;
 		    if (*cp >= '0' && *cp < '8')
 			c = c * 8 + *cp++ - '0';
