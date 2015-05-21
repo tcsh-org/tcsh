1aef46562f86e1f118649c27b390c1f0958ebb3b
Christos Zoulas <christos@zoulas.com>
2015-05-21 21:38:12 +0000

    update for 6.19.00

--- patchlevel.h
+++ patchlevel.h
@@ -1,4 +1,4 @@
-/* $Header: /p/tcsh/cvsroot/tcsh/patchlevel.h,v 3.183 2015/05/04 15:34:50 christos Exp $ */
+/* $Header: /p/tcsh/cvsroot/tcsh/patchlevel.h,v 3.184 2015/05/10 13:32:44 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
@@ -7,8 +7,8 @@
 
 #define ORIGIN "Astron"
 #define REV 6
-#define VERS 18
-#define PATCHLEVEL 5
-#define DATE "2015-05-10"
+#define VERS 19
+#define PATCHLEVEL 0
+#define DATE "2015-05-21"
 
 #endif /* _h_patchlevel */
