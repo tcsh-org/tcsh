4249ce4c91d78376ff38793e907ca9427941d1f1
Christos Zoulas <christos@zoulas.com>
2016-09-23 19:17:28 +0000

    Don't define BSDWAIT for linux anymore.

--- sh.proc.c
+++ sh.proc.c
@@ -1,4 +1,4 @@
-/* $Header: /p/tcsh/cvsroot/tcsh/sh.proc.c,v 3.132 2016/05/24 15:11:30 christos Exp $ */
+/* $Header: /p/tcsh/cvsroot/tcsh/sh.proc.c,v 3.133 2016/05/24 17:41:12 christos Exp $ */
 /*
  * sh.proc.c: Job manipulations
  */
@@ -32,7 +32,7 @@
  */
 #include "sh.h"
 
-RCSID("$tcsh: sh.proc.c,v 3.132 2016/05/24 15:11:30 christos Exp $")
+RCSID("$tcsh: sh.proc.c,v 3.133 2016/05/24 17:41:12 christos Exp $")
 
 #include "ed.h"
 #include "tc.h"
@@ -47,11 +47,9 @@ RCSID("$tcsh: sh.proc.c,v 3.132 2016/05/24 15:11:30 christos Exp $")
 # define HZ 16
 #endif /* aiws */
 
-#if defined(_BSD) || (defined(IRIS4D) && __STDC__) || defined(__lucid) || defined(__linux__) || defined(__GNU__)
-# if !defined(__ANDROID__) && !defined(__GLIBC__)
-#  define BSDWAIT
-# endif
-#endif /* _BSD || (IRIS4D && __STDC__) || __lucid || gnu-linux */
+#if defined(_BSD) || (defined(IRIS4D) && __STDC__) || defined(__lucid)
+# define BSDWAIT
+#endif /* _BSD || (IRIS4D && __STDC__) || __lucid */
 #ifndef WTERMSIG
 # define WTERMSIG(w)	(((union wait *) &(w))->w_termsig)
 # ifndef BSDWAIT
