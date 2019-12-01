b41f6367f415dd7bd0b02743a2eaa9caa855e1cb
zoulasc <christos@zoulas.com>
2019-12-01 12:02:44 -0500

    Prepare for 6.22.01.

--- patchlevel.h
+++ patchlevel.h
@@ -7,7 +7,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 22
-#define PATCHLEVEL 00
-#define DATE "2019-11-28"
+#define PATCHLEVEL 01
+#define DATE "2019-12-01"
 
 #endif /* _h_patchlevel */
