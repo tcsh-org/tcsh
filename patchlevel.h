39385f2de4449005a9e1ea8ef7f714927c2f3509
zoulasc <christos@zoulas.com>
2019-12-04 12:22:32 -0500

    Bump everything to 6.22.02

--- patchlevel.h
+++ patchlevel.h
@@ -7,7 +7,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 22
-#define PATCHLEVEL 01
-#define DATE "2019-12-01"
+#define PATCHLEVEL 02
+#define DATE "2019-12-04"
 
 #endif /* _h_patchlevel */
