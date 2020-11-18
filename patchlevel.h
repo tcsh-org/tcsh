25bf1f37728d204e75fbf5862779ff673531767f
Christos Zoulas <christos@zoulas.com>
2020-11-18 12:34:01 -0500

    ready for 6.22.03

--- patchlevel.h
+++ patchlevel.h
@@ -7,7 +7,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 22
-#define PATCHLEVEL 02
-#define DATE "2019-12-04"
+#define PATCHLEVEL 03
+#define DATE "2020-11-18"
 
 #endif /* _h_patchlevel */
