f5ca2903179138ba5170d9fad598f11e47de6468
Kimmo Suominen <kimmo@suominen.com>
2021-11-11 11:03:05 +0200

    Release 6.23.00

--- patchlevel.h
+++ patchlevel.h
@@ -6,8 +6,8 @@
 
 #define ORIGIN "Astron"
 #define REV 6
-#define VERS 22
-#define PATCHLEVEL 04
-#define DATE "2021-04-26"
+#define VERS 23
+#define PATCHLEVEL 00
+#define DATE "2021-11-11"
 
 #endif /* _h_patchlevel */
