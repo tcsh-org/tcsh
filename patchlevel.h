9818ab2cab15386def9b2e761f585162fcf22b7e
Kimmo Suominen <kimmo@suominen.com>
2022-12-03 09:17:50 +0200

    Release 6.24.02

--- patchlevel.h
+++ patchlevel.h
@@ -7,7 +7,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 24
-#define PATCHLEVEL 02
-#define DATE "2022-11-24"
+#define PATCHLEVEL 03
+#define DATE "2022-12-03"
 
 #endif /* _h_patchlevel */
